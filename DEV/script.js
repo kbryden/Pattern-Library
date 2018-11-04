6a1934c965ff1d1aade308a4f0946ef826a257c5
kbryden <kristibryden@gmail.com>
2018-11-04 15:53:23 -0700

    Updates to Modals and Form Elements
    Added Modals. Added calender to form elements

--- DEV/script.js
+++ DEV/script.js
@@ -1,43 +1,74 @@
 
+var navButton = document.querySelector(".nav-icon");
+var closeButton = document.querySelector(".close-icon");
+var modalButton = document.querySelector('.btnsimplemodal');
+var modalcloseButton = document.querySelector('.close-icon');
+
+//http://jqueryui.com/datepicker/#icon-trigger
+$("document").ready(function() {
+    
+$(function () {
+    $( "#datepicker" ).datepicker({
+      showOn: "button",
+      buttonImage: "icons/calendar.png",
+      buttonImageOnly: true,
+      buttonText: "Select date"
+    });
+  } );
+
+  $( function() {
+    $( "#datepicker2" ).datepicker();
+  } );
+
+
 // to open nav
-var navButton = document.querySelector('.nav-icon');
-navButton.addEventListener('click', function () {
-  console.log('clicky!!!');
+function toggleNav() {
+  document
+    .querySelector("body")
+    .classList
+    .toggle("nav-open");
+};
+
+//to close nav 
+function toggleNavClose() {
+    document
+      .querySelector("body")
+      .classList
+      .remove("nav-open");
+};
+
+// to open modal
+function toggleModal() {
   document
     .querySelector('body')
     .classList
-    .toggle('nav-open')
-});
+    .toggle('simple-modal-open');
+};
 
-//to close nav
-var closeButton = document.querySelector('.close-icon');
-closeButton.addEventListener('click', function() {
-    console.log('close it!');
+//to close modal
+function toggleModalClose() {
     document
       .querySelector('body')
       .classList
-      .toggle('nav-open')
+      .remove('simple-modal-open');
+};
+
+    //written in JQuery
+    
+$(navButton).click(toggleNav);
+$(closeButton).click(toggleNavClose);
+$(modalButton).click(toggleModal);
+$(modalcloseButton).click(toggleModalClose);
+    
+    //written in Vanilla JavaScript
+//navButton.addEventListener("click", toggleNav); 
+//closeButton.addEventListener("click", toggleNavClose);
+//modalButton.addEventListener("click", toggleModal);
+//modalcloseButton.addEventListener("click", toggleModalClose);
+                                  
+
 });
 
 
-// to toggle dropdown
-var navButton = document.querySelector('.dropdown-closed');
-navButton.addEventListener('click', function () {
-  console.log('clicky!!!');
-  document
-    .querySelector('body')
-    .classList
-    .toggle('dropdown-open')
-});
 
-
-//to click nav elements
-//$("document").ready(function() {
-
- // $( ".nav-hamburger ul li" ).click(function(){
- //     console.log('clicky!!!');
- //   $( ".nav-hamburger ul li div" ).toggleClass(".ul-info");
- // });
-
-//});
 
